987f167c4b56815e1fd91fe2445f37f1d0d165b9
haritha murali <hm535@cornell.edu>
2018-09-29 19:15:28 -0400

    initial commit

---
+++ nodes/human-turtle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+"""
+Script for broadcasting the turtle state to tf2
+
+Haritha Murali (hm535)
+21 September 2018
+"""
+
+import rospy
+import tf_conversions
+import tf2_ros
+import geometry_msgs.msg 
+import turtlesim.msg 
+from math import pi
+
+class leaderTurtle:
+	def __init__ (self):
+		rospy.init_node('turtle_broadcaster', anonymous = True)
+
+		# create the transform broadcaster object that will be used to send transformations
+		self.broadcaster = tf2_ros.TransformBroadcaster()
+		self.vel_publisher = rospy.Publisher('turtle1/cmd_vel', geometry_msgs.msg.Twist, queue_size=10)
+
+		# create the message object where the transformation will be stored
+		self.t = geometry_msgs.msg.TransformStamped()
+
+		# create pose subscriber
+		self.turtlename = rospy.get_param('~turtle') #each turtle has a unique name
+		subscriber = rospy.Subscriber('/%s/pose' % self.turtlename, turtlesim.msg.Pose, self.update_pose)
+		
+		self.pose = turtlesim.msg.Pose()
+		self.vel_msg = geometry_msgs.msg.Twist()
+		self.vel_msg.linear.x = 2
+		self.rate = rospy.Rate(10)
+
+	def update_pose(self, data):
+		self.pose = data
+
+	def move(self):
+		
+		
+		while not rospy.is_shutdown():
+
+			if (self.pose.x > 10 or self.pose.x < 0.5):
+				self.vel_msg.linear.x *= -1
+			
+			self.vel_publisher.publish(self.vel_msg)
+
+			self.t.header.stamp = rospy.Time.now()
+			self.t.header.frame_id = "world"
+			self.t.child_frame_id = self.turtlename
+			self.t.transform.translation.x = self.pose.x
+			self.t.transform.translation.y = self.pose.y
+			self.t.transform.translation.z = 0.0
+
+			q = tf_conversions.transformations.quaternion_from_euler(0, 0, self.pose.theta)
+			self.t.transform.rotation.x = q[0]
+			self.t.transform.rotation.y = q[1]
+			self.t.transform.rotation.z = q[2]
+			self.t.transform.rotation.w = q[3]
+
+			self.broadcaster.sendTransform(self.t)
+			self.rate.sleep()
+
+if __name__ == '__main__':
+	try: 
+		turtle = leaderTurtle()
+		turtle.move()
+		# turtle.broadcast_transform()
+	except rospy.ROSInterruptException: pass
+	rospy.spin()
---
+++ nodes/robot-turtle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+"""
+Script for listening to tf2 and following the leader turtle
+
+Haritha Murali (hm535)
+21 September 2018
+"""
+
+import rospy
+import math
+import tf2_ros
+import geometry_msgs.msg 
+import turtlesim.srv
+
+class followerTurtle:
+	def __init__(self):
+		rospy.init_node('turtle_listener')
+
+		self.transformBuffer = tf2_ros.Buffer()
+
+		# listener object that received tf2 transform messages
+		listener = tf2_ros.TransformListener(self.transformBuffer)
+
+		# spawn service creates multiple turtles
+		rospy.wait_for_service('spawn')
+		spawner = rospy.ServiceProxy('spawn', turtlesim.srv.Spawn)
+
+		# get parameters from the parameter server
+		self.turtlename = rospy.get_param('turtle', 'turtle2')
+
+		# spawn turtle at the transform specified
+		spawner(4, 4, 0, self.turtlename)
+
+		# publisher for the Twist message
+		self.publisher = rospy.Publisher('%s/cmd_vel' % self.turtlename, geometry_msgs.msg.Twist, queue_size=1)
+		self.msg = geometry_msgs.msg.Twist()
+		self.rate = rospy.Rate(10)
+
+	def move(self):
+		while not rospy.is_shutdown():
+
+			try:
+				trans = self.transformBuffer.lookup_transform(self.turtlename, 'turtle1', rospy.Time())
+			# exception for error catching
+			except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+				self.rate.sleep()
+				continue
+
+			# set angular velocity
+			self.msg.angular.z = math.atan2(trans.transform.translation.y, trans.transform.translation.x)
+
+			# set linear velocity
+			self.msg.linear.x = 0.5 * math.sqrt(trans.transform.translation.x ** 2 + trans.transform.translation.y ** 2)
+
+			# publish the velocity message
+			self.publisher.publish(self.msg)
+			self.rate.sleep()
+
+if __name__ == '__main__':
+	try: 
+		turtle = followerTurtle()
+		turtle.move()
+	except rospy.ROSInterruptException: pass
+	rospy.spin()
